f07de8764f78aa619dedf16d72057e87a6d09e3c
rnordstrom <rikard.nordstrom94@gmail.com>
2018-02-10 18:43:40 +0100

    Expand Draw2D API and add ClipRect class

--- vg_engine/vg_gfx/graphics2d.cpp
+++ vg_engine/vg_gfx/graphics2d.cpp
@@ -1,18 +1,52 @@
 #include "graphics2d.h"
+using namespace Util;
+using namespace Draw;
 
-Draw2D::Draw2D(std::string title, int width, int height)
+Util::ClipRect::ClipRect(int xPos, int yPos, int width, int height)
+{
+	x = xPos;
+	y = yPos;
+	w = width;
+	h = height;
+}
+
+int ClipRect::xPos()
+{
+	return x;
+}
+
+int ClipRect::yPos()
+{
+	return y;
+}
+
+int ClipRect::width()
+{
+	return w;
+}
+
+int ClipRect::height()
+{
+	return h;
+}
+
+Draw2D::Draw2D(const std::string & title, int width, int height)
 {
 	window.create(sf::VideoMode(width, height), title);
 }
 
-void Draw2D::drawImage(sf::Sprite sprite)
+/*	Draw sprite on screen. Use as a helper method to implement other
+	drawing methods or within the framework by creating sprites from
+	memory-loaded textures. */
+void Draw2D::drawImage(const sf::Sprite & sprite)
 {
 	window.clear();
 	window.draw(sprite);
 	window.display();
 }
 
-void Draw2D::drawImage(std::string fileName)
+/*	Load texture from file */
+void Draw2D::drawImage(const std::string & fileName)
 {
 	sf::Texture texture;
 
@@ -27,17 +61,36 @@ void Draw2D::drawImage(std::string fileName)
 	drawImage(sprite);
 }
 
-void Draw2D::drawImage(std::string fileName, int x, int y, int w, int h)
+/*	Load texture from file and place at absolute position */
+void Draw2D::drawImage(const std::string & fileName, int xAbs, int yAbs)
 {
 	sf::Texture texture;
 
-	if (!texture.loadFromFile(fileName, sf::IntRect(x, y, w, h)))
+	if (!texture.loadFromFile(fileName))
 	{
 		// TODO Handle error
 	}
 
 	sf::Sprite sprite;
 	sprite.setTexture(texture);
+	sprite.move(sf::Vector2f(xAbs, yAbs));
+
+	drawImage(sprite);
+}
+
+/*	Load texture from file, clip a sprite and place at absolute position */
+void Draw2D::drawImage(const std::string & fileName, ClipRect & clip, int xAbs, int yAbs)
+{
+	sf::Texture texture;
+
+	if (!texture.loadFromFile(fileName, sf::IntRect(clip.xPos(), clip.yPos(), clip.width(), clip.height())))
+	{
+		// TODO Handle error
+	}
+
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+	sprite.move(sf::Vector2f(xAbs, yAbs));
 
 	drawImage(sprite);
 }
--- vg_engine/vg_gfx/graphics2d.h
+++ vg_engine/vg_gfx/graphics2d.h
@@ -2,14 +2,37 @@
 #include <string>
 #include <SFML/Graphics.hpp>
 
-/* Used for drawing 2D sprites. */
-class Draw2D
+namespace Util
 {
-private:
-	sf::RenderWindow window;
-	void drawImage(sf::Sprite sprite);
-public:
-	Draw2D(std::string title, int width = 800, int height = 600);
-	void drawImage(std::string fileName);
-	void drawImage(std::string fileName, int x, int y, int w, int h);
-};
+	/*	Utility class for grouping sprite clip values */
+	class ClipRect
+	{
+	private:
+		int x;
+		int y;
+		int w;
+		int h;
+	public:
+		ClipRect(int xPos, int yPos, int width, int height);
+		int xPos();
+		int yPos();
+		int width();
+		int height();
+	};
+}
+
+namespace Draw
+{
+	/*	Used for drawing 2D sprites. */
+	class Draw2D
+	{
+	private:
+		sf::RenderWindow window;
+		void drawImage(const sf::Sprite & sprite);
+	public:
+		Draw2D(const std::string & title, int width = 800, int height = 600);
+		void drawImage(const std::string & fileName);
+		void drawImage(const std::string & fileName, int xAbs = 0, int yAbs = 0);
+		void drawImage(const std::string & fileName, Util::ClipRect & clip, int xAbs = 0, int yAbs = 0);
+	};
+}
